f7916982b030af5599189c4e1684ea9c5209a4f3
Joshua Buchacher <jbuchacher@ubermind.com>
2013-01-01 16:22:48 -0800

    Added bz_session model

---
+++ Bezel/Model/bz_Session.h
@@ -0,0 +1,13 @@
+//
+//  bz_Session.h
+//  Bezel
+//
+//  Created by Josh Buchacher on 1/1/13.
+//  Copyright (c) 2013 Mind Diaper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface bz_Session : NSObject
+
+@end
---
+++ Bezel/Model/bz_Session.m
@@ -0,0 +1,13 @@
+//
+//  bz_Session.m
+//  Bezel
+//
+//  Created by Josh Buchacher on 1/1/13.
+//  Copyright (c) 2013 Mind Diaper. All rights reserved.
+//
+
+#import "bz_Session.h"
+
+@implementation bz_Session
+
+@end
