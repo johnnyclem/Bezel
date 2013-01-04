2e5d28ed112a8075f5db45e9692342cb58f72993
Joshua Buchacher <jbuchacher@ubermind.com>
2013-01-03 16:42:12 -0800

    Added BZAdjustmentProcessor for processing adjustments on images.

---
+++ Bezel/Controller/BZAdjustmentProcessor/BZAdjustmentProcessor.h
@@ -0,0 +1,13 @@
+//
+//  BZAdjustmentProcessor.h
+//  Bezel
+//
+//  Created by Josh Buchacher on 1/3/13.
+//  Copyright (c) 2013 Mind Diaper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BZAdjustmentProcessor : NSObject
+
+@end
---
+++ Bezel/Controller/BZAdjustmentProcessor/BZAdjustmentProcessor.m
@@ -0,0 +1,13 @@
+//
+//  BZAdjustmentProcessor.m
+//  Bezel
+//
+//  Created by Josh Buchacher on 1/3/13.
+//  Copyright (c) 2013 Mind Diaper. All rights reserved.
+//
+
+#import "BZAdjustmentProcessor.h"
+
+@implementation BZAdjustmentProcessor
+
+@end
