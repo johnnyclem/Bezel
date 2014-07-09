6e518a4e17ba6c01876bcc5a9e375cc24c69690e
John Clem <johnnyclem@gmail.com>
2014-07-09 12:37:59 -0700

    add image assets/icons and basic CoreImage overlay example

--- Bezel/ViewController.swift
+++ Bezel/ViewController.swift
@@ -2,24 +2,82 @@
 //  ViewController.swift
 //  Bezel
 //
-//  Created by Bradley Johnson on 7/9/14.
-//  Copyright (c) 2014 learnswift. All rights reserved.
+//  Created by John Clem on 7/9/14.
+//  Copyright (c) 2014 Mind Diaper. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-                            
+    
+    @IBOutlet var imageView : UIImageView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        imageView!.contentMode = UIViewContentMode.ScaleAspectFill
+        var image = UIImage(named: "road.jpg")
+        
+        
+        imageView!.image = image
+        self.testImageViewOverlay()
+    }
+    
+    func testImageViewOverlay() {
+        //        let overlay = UIImage(named: "bolt_black.png")
+        //        let overlayView = UIImageView(frame: imageView!.frame)
+        //        overlayView.image = overlay
+        //        imageView!.addSubview(overlayView)
+        
+        let composition = self.overlayImageWithShape(nil, image: imageView!.image)
+        imageView!.image = UIImage(CIImage: composition)
+    }
+    
+    func overlayBoltByCheating() {
+        let overlayImage = UIImage(named: "bolt_black.png")
+        let overlay = UIImageView(frame: imageView!.frame)
+        overlay.contentMode = UIViewContentMode.ScaleAspectFit
+        overlay.image = overlayImage
+        self.view.addSubview(overlay)
+    }
+    
+    func overlayImageWithShape(shape : Shape?, image: UIImage) -> CIImage {
+        let context = CIContext(options: nil)
+        
+        let background = CIImage(image: image)
+        let overlayImage = UIImage(named: "bolt_black.png")
+        let overlay = CIImage(image: overlayImage)
+        
+        return overlay.imageByCompositingOverImage(background)
+    }
+    
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
