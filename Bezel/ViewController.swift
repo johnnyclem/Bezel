e0c26cb60a0997a97318eeb61a6e8596769820d7
bradleypj@icloud.com <Brad@Bradleys-MacBook-Pro.local>
2014-07-09 18:38:40 -0700

    -added a scrollview to the interface -added uiimagepicker for the user to pick photos from the camera or photo library -selected photo populates an image view on the scrollview, which now scrolls with panning

--- Bezel/ViewController.swift
+++ Bezel/ViewController.swift
@@ -8,47 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     
-    @IBOutlet var imageView : UIImageView?
+    var imageView : UIImageView!
+    @IBOutlet strong var scrollView: UIScrollView?
     @IBOutlet var collectionView : UICollectionView?
     var dataSource : BezelCollectionViewDataSource?
     let actionController = UIAlertController(title: "Image Source", message: "Select Your Choice Please", preferredStyle: UIAlertControllerStyle.ActionSheet)
+    let cameraPicker = UIImagePickerController()
+    let libraryPicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.testCollectionViewDataSource()
-        self.setupActionController()
+        self.setupPickersAndActionController()
+        
+        self.imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: self.scrollView!.frame.size.width, height: self.scrollView!.frame.size.height))
+        self.scrollView!.addSubview(self.imageView)
+        
+        imageView!.contentMode = UIViewContentMode.ScaleAspectFill
+        var image = UIImage(named: "road.jpg")
+        imageView!.image = image
     }
     
-    func setupActionController(){
+    func setupPickersAndActionController(){
+        
+        //only add the camera option if a camera is on the device
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            self.cameraPicker.delegate = self
+            self.cameraPicker.allowsEditing = false
+            self.cameraPicker.sourceType = UIImagePickerControllerSourceType.Camera
+            let cameraOption = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default, handler: {(action :UIAlertAction!) -> Void in
+                self.presentViewController(self.cameraPicker, animated: true, completion: nil)
+                })
+             self.actionController.addAction(cameraOption)
+        }
+        
+        self.libraryPicker.delegate = self
+        self.libraryPicker.allowsEditing = false
+        self.libraryPicker.sourceType = UIImagePickerControllerSourceType.SavedPhotosAlbum
+        let libraryOption = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default, handler: {(action :UIAlertAction!) -> Void in
+            self.presentViewController(self.libraryPicker, animated: true, completion: nil)
+            })
+        
         
         let cancelOption = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: {(action :UIAlertAction!) -> Void in
             println(action.title)
             })
-        let cameraOption = UIAlertAction(title: "Camera", style: UIAlertActionStyle.Default, handler: {(action :UIAlertAction!) -> Void in
-            println(action.title)
-            })
-        let libraryOption = UIAlertAction(title: "Photo Library", style: UIAlertActionStyle.Default, handler: {(action :UIAlertAction!) -> Void in
-            println(action.title)
-            })
-        self.actionController.addAction(cameraOption)
         self.actionController.addAction(cancelOption)
         self.actionController.addAction(libraryOption)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        imageView!.contentMode = UIViewContentMode.ScaleAspectFill
-        var image = UIImage(named: "road.jpg")
-        
-        
-        imageView!.image = image
-//        self.testImageViewOverlay()
     }
     
     func testImageViewOverlay() {
@@ -88,6 +100,17 @@ class ViewController: UIViewController {
     
     @IBAction func cameraButtonPressed(sender: AnyObject) {
         self.presentViewController(self.actionController, animated: true, completion: nil)
+    }
+    
+    //#pragma mark - UIImagePickerControllerDelegate
+    
+     func imagePickerController(picker: UIImagePickerController!,
+        didFinishPickingMediaWithInfo info: [NSObject : AnyObject]!) {
+            let image = info[UIImagePickerControllerOriginalImage] as UIImage
+            self.imageView.frame.size = image.size
+            self.scrollView!.contentSize = image.size
+            self.imageView.image = image
+            self.dismissViewControllerAnimated(true, completion: nil)
     }
 }
 
